9794b95c7152f36c514ef3cfe89b3ec0b0539d55
dplusplus <sulpsulpd@gmail.com>
2015-09-28 21:06:53 +0900

    attitude-change.ccの処理を日本語化

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -148,8 +148,8 @@ static TextDB AllDBs[] =
            "jtrans_arena.txt",
            "jtrans_art_func.txt",
            "jtrans_attack.txt",
-           /*
            "jtrans_attitude_change.txt",
+           /*
            "jtrans_beam.txt",
            */
            "jtrans_behold.txt",
