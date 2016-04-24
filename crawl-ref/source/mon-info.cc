839619c9549cc7e9733de625eab47728527a724f
dplusplus <sulpsulpd@gmail.com>
2016-04-24 19:13:21 +0900

    キメラの英名に日本語が混じるのとxxx's yyy headの訳見直し

--- crawl-ref/source/mon-info.cc
+++ crawl-ref/source/mon-info.cc
@@ -1149,7 +1149,7 @@ string monster_info::common_name(description_level_type desc) const
             && (me = get_monster_data(u.ghost.acting_part)))
         {
             // Specify an acting head
-            ss << "の持つ" << me->name << "の頭";
+            ss << "に生えた" << me->name << "の頭";
         }
         else
             // Suffix parts in brackets
@@ -1267,7 +1267,7 @@ string monster_info::common_name_en(description_level_type desc) const
             && (me = get_monster_data(u.ghost.acting_part)))
         {
             // Specify an acting head
-            ss << "に生えた" << me->name << "の頭";
+            ss << "'s " << me->name << " head";
         }
         else
             // Suffix parts in brackets
