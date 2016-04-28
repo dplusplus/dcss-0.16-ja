bf604bb882414ac289c921a249889d4505bda77a
dplusplus <sulpsulpd@gmail.com>
2016-04-29 03:59:46 +0900

    food.ccのメッセージを日本語化

--- crawl-ref/source/food.cc
+++ crawl-ref/source/food.cc
@@ -1596,7 +1596,7 @@ int hunger_bars(const int hunger)
 string hunger_cost_string(const int hunger)
 {
     if (you_foodless(true))
-        return "N/A";
+        return jtrans("N/A");
 
 #ifdef WIZARD
     if (you.wizard)
@@ -1611,5 +1611,5 @@ string hunger_cost_string(const int hunger)
                + string(ARRAYSZ(hunger_breakpoints) - numbars, '.');
     }
     else
-        return "None";
+        return jtrans("None");
 }
