f0dee502f26f5007610d9126a1d31ff1239e865b
dplusplus <sulpsulpd@gmail.com>
2016-05-14 11:42:24 +0900

    モンスターの呪文詠唱メッセージで"you"がそのままになっているのを修正

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -7176,7 +7176,7 @@ void mons_cast_noise(monster* mons, const bolt &pbolt,
     if (targeted)
         _speech_fill_target(targ_prep, target, mons, pbolt, gestured);
 
-    msg = replace_all(msg, "@target@", tagged_jtrans("[zap]", target));
+    msg = replace_all(msg, "@target@", jtrans(target));
 
     string beam_name;
     if (!targeted)
