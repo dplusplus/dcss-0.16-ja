560852833619e7c84af4cf10add902b165ec550f
dplusplus <sulpsulpd@gmail.com>
2018-01-15 23:44:44 +0900

    不要なコメントアウト箇所削除

--- crawl-ref/source/database.cc
+++ crawl-ref/source/database.cc
@@ -161,9 +161,6 @@ static TextDB AllDBs[] =
            "jtrans_describe.txt",
            "jtrans_dgn_overview.txt",
            "jtrans_directn.txt",
-           /*
-           "jtrans_effects.txt",
-           */
            "jtrans_evoke.txt",
            /*
            "jtrans_exclude.txt",
